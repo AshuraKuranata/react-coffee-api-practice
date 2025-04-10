2371164276983dad33a30a633f6407ff9cfb16a7
JohnNg-GA <john.q.ng@gmail.com>
2025-04-10 11:04:52 -0700

    git init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Coffee REACT connect to my API Lab</h1>
+    </>
+  )
+}
+
+export default App
